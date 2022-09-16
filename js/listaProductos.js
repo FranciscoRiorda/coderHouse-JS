1caeaeddfdb4ebfbb41a0d647ee72805ebd06da7
Francisco Riorda <riorda7@gmail.com>
2022-09-16 14:33:33 -0300

    Manipulación de DOM

---
+++ js/listaProductos.js
@@ -0,0 +1,74 @@
+const listaProductos = [
+    {
+        id: 1,
+        categoria: 'RECETADOS',
+        modelo: 'GOTTE',
+        precio: 11000,
+        img: '/images/rusty/gotte.jpg',
+        cantidad: 1
+    },
+    {
+        id: 2,
+        categoria: 'RECETADOS',
+        modelo: 'VIENET',
+        precio: 15000,
+        img: '/images/rusty/vienet.jpg',
+        cantidad: 1
+    },
+    {
+        id: 3,
+        categoria: 'RECETADOS',
+        modelo: 'THER',
+        precio: 18500,
+        img: '/images/rusty/ther.jpg',
+        cantidad: 1
+    },
+    {
+        id: 4,
+        categoria: 'SOL',
+        modelo: 'CALLED',
+        precio: 18000,
+        img: '/images/rusty/called.jpg',
+        cantidad: 1
+    },
+    {
+        id: 5,
+        categoria: 'SOL',
+        modelo: 'CREDIX',
+        precio: 20000,
+        img: '/images/rusty/credix.jpg',
+        cantidad: 1
+    },
+    {
+        id: 6,
+        categoria: 'SOL',
+        modelo: 'SPELL',
+        precio: 25000,
+        img: '/images/rusty/spell.jpg',
+        cantidad: 1
+    },
+    {
+        id: 7,
+        categoria: 'DEPORTIVOS',
+        modelo: 'SOTION',
+        precio: 24000,
+        img: '/images/rusty/sotion.jpg',
+        cantidad: 1
+    },
+    {
+        id: 8,
+        categoria: 'DEPORTIVOS',
+        modelo: 'ESLAV',
+        precio: 30000,
+        img: '/images/rusty/eslav.jpg',
+        cantidad: 1
+    },
+    {
+        id: 9,
+        categoria: 'DEPORTIVOS',
+        modelo: 'YAU',
+        precio: 21000,
+        img: '/images/rusty/yau.jpg',
+        cantidad: 1
+    },
+];
