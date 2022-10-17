49f12f4e806b0fec3a76aa9cfd798bea5b7415a0
Francisco Riorda <riorda7@gmail.com>
2022-10-17 14:00:59 -0300

    Optimización fin de proyecto

--- src/controllers/listaProductosController.js
+++ src/controllers/listaProductosController.js
@@ -1,6 +1,6 @@
 const listaProductoController = async () => {
     try {
-        const response = await fetch('./src/data/listaProductos.json');
+        const response = await fetch('../src/data/listaProductos.json');
         const data = await response.json();
 
         return data;
